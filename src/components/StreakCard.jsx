171c70e330106dc1db12a073c1c9f13d86ff022f
BJ008Creative <baishalinx@gmail.com>
2026-08-16 16:31:55 +0530

    put a input

--- src/components/StreakCard.jsx
+++ src/components/StreakCard.jsx
@@ -1,4 +1,6 @@
 function StreakCard(){
-    return(<><h1>DSA learning</h1></>);
+    return(<><h1>DSA learning</h1>
+    <input placeholder="Write your goal here"></input>
+    </>);
 }
 export {StreakCard};
