e4acdd51ca5fe9b0b2265007a6b40e7d146bb920
BJ008Creative <baishalinx@gmail.com>
2026-09-11 15:09:05 +0530

    added more fucntionality to app

--- src/components/AddProp.jsx
+++ src/components/AddProp.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 function AddProp(props) {
+
+    const [goalName, setGoalName] = useState("");
+
     return (
-        <button onClick={props.addGoal}>
-            Add Streak
-        </button>
+        <>
+            <input
+                value={goalName}
+                onChange={(event) => setGoalName(event.target.value)}
+                placeholder="Enter goal name"
+            />
+
+            <button onClick={props.addGoal}>
+                Add Streak
+            </button>
+        </>
     );
 }
 
